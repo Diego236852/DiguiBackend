403951521732e77d9580959da8381483b463bc3b
JosiasJSosaS <josiasjsosas@gmail.com>
2024-10-16 16:08:53 -0600

    penis

---
+++ routes/database.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const fs = require('node:fs');
+
+app = express();
+
+app.use(express.json());
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    
+    let response = {
+        textoReal: "Yo es gay"
+    }
+
+    res.render("test")
+});
+
+router.post('/adduser', (req, res) => {
+    let input = req.body;
+
+    fs.writeFile('/home/ubuntu/file', JSON.stringify(input), err => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.send("Se agrego correctamente");
+        }
+    });
+
+});
+
+module.exports = router;
